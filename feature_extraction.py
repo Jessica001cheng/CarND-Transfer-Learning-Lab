8577793b15911ca2d365e8e05a64a9d9adc536f7
Lingyu Cheng <Lingyu.Cheng@continental-corporation.com>
2018-07-16 09:03:21 +0800

    3rd version

--- feature_extraction.py
+++ feature_extraction.py
@@ -10,6 +10,8 @@ FLAGS = flags.FLAGS
 # command line flags
 flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
 flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
+flags.DEFINE_string('batch_size', 128, "Batch size, default is 128")
+flags.DEFINE_string('epochs', 1, "Epochs, default is 1")
 
 
 def load_bottleneck_data(training_file, validation_file):
@@ -50,9 +52,9 @@ def main(_):
     # the dataset
     # 10 for cifar10
     # 43 for traffic
-    n_class = 10
-    batch_size = 128
-    epochs = 1
+    n_class = len(np.unique(y_train))
+    batch_size = FLAGS.batch_size
+    epochs = FLAGS.epochs
     input_shape = X_train.shape[1:]
     print("input shape: ", input_shape)
     input = Input(shape = input_shape)
@@ -61,7 +63,7 @@ def main(_):
     model = Model(inputs=input, outputs=output)
     # TODO: train your model here
     model.compile(optimizer="Adam", loss="sparse_categorical_crossentropy", metrics=['accuracy'])
-    model.fit(x=X_train, y=y_train, batch_size=batch_size, epochs=epochs, validation_date=(X_val, y_val), shuffle=true)
+    model.fit(x=X_train, y=y_train, batch_size=batch_size, epochs=epochs, validation_date=(X_val, y_val), shuffle=True)
 
 # parses flags and calls the `main` function above
 if __name__ == '__main__':
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-python feature_extraction.py --training_file vgg_cifar10_100_bottleneck_features_train.p --validation_file vgg_cifar10_bottleneck_features_validation.p
+source activate carnd-term1
+python feature_extraction.py --training_file vgg_cifar10_100_bottleneck_features_train.p --validation_file vgg_cifar10_bottleneck_features_validation.p --batch_size 128 --epochs 10
